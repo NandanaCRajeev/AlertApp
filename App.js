2c616a15dc4e6da859288082657889562fdd4d84
NandanaCRajeev <83021680+NandanaCRajeev@users.noreply.github.com>
2021-10-22 18:49:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Alert App</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text>Buzzer</Text>
+
+      </TouchableOpacity>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button:{
+    width:200,
+    height:200,
+    borderRadius:25,
+    backgroundColor:"blue",
+  }
+});
